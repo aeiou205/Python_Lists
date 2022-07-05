18ee9ab8d341a34e311068108a26ea23aa646006
aeiou205 <106573426+aeiou205@users.noreply.github.com>
2022-07-04 23:40:28 -0500

    Add files via upload
    creating list of lists on python

---
+++ s7_list_of_lists.py
@@ -0,0 +1,18 @@
+#list of list
+#Daniel S. Nava
+#Python lest create list of list 
+
+#variables house and components in square meters
+
+hall = 22.3
+kit = 23.5	
+liv = 22.55
+bath = 23.33
+
+house = [["HALL",hall],
+	 ["KITCHEN", kit],
+	 ["LIV", liv],
+	 ["BATH", bath],
+	]
+	
+print("Values of home", house) 
