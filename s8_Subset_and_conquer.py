52fa90eedd99953f6c8a39991a0d5cb3dddd12d6
aeiou205 <106573426+aeiou205@users.noreply.github.com>
2022-07-05 23:12:27 -0500

    Add files via upload
    Subset and conquer in python

---
+++ s8_Subset_and_conquer.py
@@ -0,0 +1,23 @@
+#create a list areas with elements
+#Autor: Daniel S. Nava
+
+areas = ["hallaway",
+	11.25,
+	"Kitchen",
+	18.0,
+	"living room",
+	20.00,
+	"bedroom",
+	10.75,
+	"bathroom",
+	9.50
+	]
+
+# Print out second element from areas(
+print(areas[2])
+#print out last element 
+print(areas[-1])
+#print out living room  
+print (areas[5])
+#print out type data areas
+print (type(areas))
