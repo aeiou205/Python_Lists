f120d256b8a3e97f0aef7dd9334c90f413f47a2a
aeiou205 <106573426+aeiou205@users.noreply.github.com>
2022-07-07 22:42:44 -0500

    Add files via upload

---
+++ s11_slicing_and_dicing.py
@@ -0,0 +1,14 @@
+#Slicing and dicing 
+#Autor: Daniel Suarez Nava
+
+# first we create a list
+hause = ["kitchen",4.40,"Bedroom",5.0," Batroom", 3.0 ]
+
+new_hause = [hause[2:4]]
+
+print (new_hause)
+
+
+ 
+# Use slicing to create downstairs 
+
