a6d23be7def036c5def6382037741f1d973151e2
aeiou205 <106573426+aeiou205@users.noreply.github.com>
2022-07-07 22:33:29 -0500

    Add files via upload

---
+++ s10_subset_and_calculate.py
@@ -0,0 +1,13 @@
+# Create the areas list
+#autor Daniel S. Nava
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Sum of kitchen and bedroom area: eat_sleep_area
+eat_sleep_area = areas[3] + areas[7]
+
+print(eat_sleep_area)
+
+
+
+
+# Print the variable eat_sleep_area
