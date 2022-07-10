3a703aceecf6ec88c42e9c479e0ab4bf443efbea
aeiou205 <106573426+aeiou205@users.noreply.github.com>
2022-07-09 23:29:27 -0500

    Add files via upload

---
+++ s12_Replace_list_elements.py
@@ -0,0 +1,13 @@
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+x = [ "a", "b", "c", "d"]
+x[1] = "r" 
+x[2:] = ["s", "t"]
+# Correct the bathroom area
+areas[9]= 23.00
+
+# Change "living room" to "chill zone"
+areas[4] = 'chill zone'
+
+print (areas)
+print (x)
