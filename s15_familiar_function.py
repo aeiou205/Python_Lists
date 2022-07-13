1b8324bde7721f7c809718855cffb4c1d49c3fbd
aeiou205 <106573426+aeiou205@users.noreply.github.com>
2022-07-13 01:05:23 -0500

    Add files via upload
    function familiar as len() , print() , type(), int()

---
+++ s15_familiar_function.py
@@ -0,0 +1,17 @@
+#function:
+#print, type() str(), int() bool() and float()
+#create varibles var1 and var2
+
+var1 = [1, 2, 3, 4]
+var2 = True
+result = type (3.0)
+
+#print out type of var1
+print(type(var1))
+
+#print out length of var1
+print(len(var1))
+
+#convert var2 to an integer: out2
+out2 = int(var2)
+print (out2)
